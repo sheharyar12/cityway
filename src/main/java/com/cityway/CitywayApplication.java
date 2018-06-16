4cd65ac8a52c772032ade9ffba5e1cda8e0faa61
Shehar Yar <syar0052@gmail.com>
2018-06-15 21:34:20 -0500

    init project

---
+++ src/main/java/com/cityway/CitywayApplication.java
@@ -0,0 +1,12 @@
+package com.cityway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitywayApplication.class, args);
+	}
+}
